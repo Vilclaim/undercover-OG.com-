70b2b323e84fb7f2be2c0810f4945ceafbf52624
Vilclaim <calimvilandrew631@gmail.com>
2026-06-12 12:07:22 +0400

    fix gmail verify

--- server.js
+++ server.js
@@ -25,12 +25,20 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+
+service: "gmail",
+
+auth: {
+
+user: process.env.EMAIL_USER,
+
+pass: process.env.EMAIL_PASS
+
+}
+
 });
+
+
 
 transporter.verify((error, success) => {
 
