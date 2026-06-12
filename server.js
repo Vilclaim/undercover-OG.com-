c4caab0fc8b389cdbbf2bb607e057f5da3e7be5c
Vilclaim <calimvilandrew631@gmail.com>
2026-06-12 14:28:44 +0400

    fix gmail smtp

--- server.js
+++ server.js
@@ -26,23 +26,19 @@ const nodemailer = require("nodemailer");
 const app = express();
 const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+  host: "smtp.gmail.com",
+
+  port: 587,
+
+  secure: false,
 
   auth: {
-
     user: process.env.EMAIL_USER,
-
     pass: process.env.EMAIL_PASS
-
-  },
-
-  tls: {
-
-    rejectUnauthorized: false
-
   }
 
 });
+
 console.log("EMAIL USER =", process.env.EMAIL_USER);
 console.log("EMAIL PASS EXISTS =", !!process.env.EMAIL_PASS);
 
