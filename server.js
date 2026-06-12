903b12846e21d9b613b03e08a195b11ab9de73e4
Vilclaim <calimvilandrew631@gmail.com>
2026-06-12 15:23:16 +0400

    smtp port 465

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ const nodemailer = require("nodemailer");
 const app = express();
 const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+  host: "smtp.gmail.com",
+
+  port: 465,
+
+  secure: true,
 
   auth: {
     user: process.env.EMAIL_USER,
